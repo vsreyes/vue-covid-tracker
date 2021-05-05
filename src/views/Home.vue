cbdb4bdb9a5feed36a1b1cce7d856f3e8570908f
vsreyes <virgilio.s.reyes@gmail.com>
2021-05-05 19:38:35 +0200

    Fetching data setup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,10 +7,17 @@
 <script>
 export default {
   name: 'Home',
-  components: {
-  created() {
-    console.log(123)
+  components: {},
+  methods: {
+    async fetchCovidData() {
+      const res = await fetch('https://api.covid19api.com/summary')
+      const data = await res.json()
+      return data
+    }
+  },
+  async created() {
+    const data = await this.fetchCovidData()
+    console.log(data)
     },
   }
-}
 </script>
