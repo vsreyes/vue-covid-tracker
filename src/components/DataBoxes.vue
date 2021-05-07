1bdde1f26dc7899bf340a72694a91184dc6b850d
vsreyes <virgilio.s.reyes@gmail.com>
2021-05-07 21:18:20 +0200

    Small corrections

--- src/components/DataBoxes.vue
+++ src/components/DataBoxes.vue
@@ -35,7 +35,7 @@ export default {
   props: ['stats'],
   methods: {
     numberWithCommas(x) {
-      return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+      return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.')
     }
   }
 }
