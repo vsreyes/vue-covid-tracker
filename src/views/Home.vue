a4b089f471f3000a7bb2e3d2578599038e91da55
vsreyes <virgilio.s.reyes@gmail.com>
2021-05-08 16:24:12 +0200

    Set country data setup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,8 @@
     <DataTitle :text="title" :dataDate="dataDate" />
 
     <DataBoxes :stats="stats" />
+
+    <CountrySelect @get-country="getCountryData" :countries="countries" />
   </main>
 
   <main class="flex flex-col align-center justify-center text-center" v-else>
@@ -16,12 +18,14 @@
 <script>
 import DataTitle from '@/components/DataTitle'
 import DataBoxes from '@/components/DataBoxes'
+import CountrySelect from '@/components/CountrySelect'
 
 export default {
   name: 'Home',
   components: {
     DataTitle,
-    DataBoxes
+    DataBoxes,
+    CountrySelect
   },
   data() {
     return {
@@ -38,6 +42,10 @@ export default {
       const res = await fetch('https://api.covid19api.com/summary')
       const data = await res.json()
       return data
+    },
+    getCountryData(country) {
+      this.stats = country
+      this.title = country.Country
     }
   },
   async created() {
